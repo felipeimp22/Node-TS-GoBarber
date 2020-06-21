cc81125ea4ca7c1d8f2ac762091e825b984a038f
Imperio Imperio <felipeimperio.imperioo@gmail.com>
2020-06-20 23:57:11 -0300

    decoupling of responsability of data methods in route to repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,37 +1,55 @@
 import { Router } from 'express';
+//--------------------------------------------------------------------------
+// coment 1
 // import { uuid } from 'uuidv4';
+//--------------------------------------------------------------------------
+
 import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+// import Appointment from '../models/Appointment';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
+//--------------------------------------------------------------------------
+// coment 1
 // interface Appointment {
 //   id: string;
 //   provider: string;
 //   date: Date;
 // }
-const appointments: Appointment[] = [];
+//--------------------------------------------------------------------------
+// coment 2
+// const appointments: Appointment[] = [];
 
+const appointmentsRepository = new AppointmentsRepository();
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  //--------------------------------------------------------------------------
+  // coment 2
+  // const findAppointmentInSameDate = appointments.find(appointment =>
+  //   isEqual(parsedDate, appointment.date),
+  // );
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
-
   if (findAppointmentInSameDate) {
     return res.status(400).json({ error: 'this date is already booked' });
   }
-
+  //--------------------------------------------------------------------------
+  // coment 1
   // const appointment = {
   //   id: uuid(),
   //   provider,
   //   date: parsedDate,
   // };
-  const appointment = new Appointment(provider, parsedDate);
-  appointments.push(appointment);
+  //--------------------------------------------------------------------------
+  // coment 2
+  // const appointment = new Appointment(provider, parsedDate);
+  // appointments.push(appointment);
 
+  const appointment = appointmentsRepository.create(provider, parsedDate);
   return res.json(appointment);
 });
 
