014023d736deb53e6fbab6bd61ea291cb4f6f0cc
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-07-18 22:45:10 -0300

    create Appointment model and config strictPropertyInialization in tscofig

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,10 +1,15 @@
-import { uuid } from 'uuidv4';
+// import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column() // se nao passar nada no () o tipo é varchar
   provider: string;
 
+  @Column('timestamp with local time zone')
   date: Date;
 
   // Omit é um Helper que passa como primeiro parametro a tipagem que voce vai usar,
@@ -14,11 +19,16 @@ class Appointment {
   // aqui no caso foi usado o proprio Appointment que tem o id,provider e date e o id é retirado
   // para que o constructor nao espere receber o id
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  /**
+   *
+   * Se o model for usado com TypeORM ou algo do tipo, o constructor nao é mais necessario pois ele já
+   * cria automaticamente por baixo dos panos
+   */
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
