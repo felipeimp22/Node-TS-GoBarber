d5f0cc151ae43a6faf41b07d36776ff1ab0be03d
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-08-26 01:59:20 -0300

    feature/generate_Token_JWT

---
+++ src/service/AuthenticateUserService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm';
+// import { hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+interface Response {
+  user: User;
+  token: string;
+}
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    // Promise < { user: User } >
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('invalid email or password');
+    }
+    // user.password = senha criptografada
+    // senha que tentou fazer login, não esta criptografada
+    const passWordMatched = await compare(password, user.password);
+    if (!passWordMatched) {
+      throw new Error('invalid email or password');
+    }
+
+    const token = sign({}, 'teste', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return {
+      user,
+      token,
+    };
+  }
+}
+export default AuthenticateUserService;
